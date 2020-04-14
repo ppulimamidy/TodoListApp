a391a389cd1dfee41c16ba00b7bb72cadecdeaa0
Pranathi Pulimamidy <ppulimamidy@gmail.com>
2020-04-14 13:49:07 -0400

    Initial Commit

---
+++ TodoListApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TodoListApp
+//
+//  Created by Pranathi Pulimamidy on 4/14/20.
+//  Copyright © 2020 Pranathi Pulimamidy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TodoListApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TodoListApp
+//
+//  Created by Pranathi Pulimamidy on 4/14/20.
+//  Copyright © 2020 Pranathi Pulimamidy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
